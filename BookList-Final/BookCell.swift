d4d27e9e25b00e4c89d0eb59926fd7474217784e
Tyler mui <tylermui1103@gmail.com>
2025-04-17 21:19:47 -0400

    files

---
+++ BookList-Final/BookCell.swift
@@ -0,0 +1,23 @@
+//
+//  BookCell.swift
+//  BookList-Final
+//
+//  Created by Tyler Mui on 4/17/25.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
