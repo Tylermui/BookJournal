444bf2501f746d9c97acdb72db829db2cfd9a259
Tyler mui <tylermui1103@gmail.com>
2025-04-17 21:20:24 -0400

    Update README.md

--- BookList-Final/BookComposeViewController.swift
+++ BookList-Final/BookComposeViewController.swift
@@ -2,20 +2,66 @@
 //  BookComposeViewController.swift
 //  BookList-Final
 //
-//  Created by Tyler Mui on 4/17/25.
+//  Created by Tyler Mui on 4/15/25.
 //
 
 import UIKit
 
 class BookComposeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var bookToEdit: Book?
+    var book: Book!
+    var onComposeBook: ((Book) -> Void)? = nil
+    @IBOutlet weak var summaryField: UITextField!
+    @IBOutlet weak var bookTitleField: UITextField!
+    
+    @IBAction func tapDoneButton(_ sender: Any) {
+        guard let title = bookTitleField.text,
+              !title.isEmpty
+        else {
+            presentAlert(title: "Oops...", message: "Make sure to add a title!")
+            return
+        }
+        
+        var book: Book
+        
+        if let editBook = bookToEdit {
+            book = editBook
+            book.title = title
+            book.summary = summaryField.text
+        } else {
+            book = Book(title: title,
+//                        status: ReadingStatus,
+                        summary: summaryField.text)
+        }
+        onComposeBook?(book)
+        
+        dismiss(animated: true)
+    }
+    @IBAction func tapBackButton(_ sender: Any) {
+        dismiss(animated: true)
     }
     
-
+    @IBAction func tapFavoriteButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+//        if sender.isSelected {
+//            book.addToFavorites()
+//        } else {
+//            book.removeFromFavorites()
+//        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let book = bookToEdit {
+            bookTitleField.text = book.title
+            summaryField.text = book.summary
+            
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +72,18 @@ class BookComposeViewController: UIViewController {
     }
     */
 
+    
+    private func presentAlert(title: String, message: String) {
+        // 1.
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        // 2.
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        // 3.
+        alertController.addAction(okAction)
+        // 4.
+        present(alertController, animated: true)
+    }
 }
--- BookList-Final/BookListViewController.swift
+++ BookList-Final/BookListViewController.swift
@@ -1,29 +1,91 @@
 //
-//  BookListViewController.swift
+//  ViewController.swift
 //  BookList-Final
 //
-//  Created by Tyler Mui on 4/17/25.
+//  Created by Tyler Mui on 4/6/25.
 //
 
 import UIKit
 
 class BookListViewController: UIViewController {
 
+    @IBOutlet weak var EmptyStateLabel: UILabel!
+    @IBOutlet weak var BookTableView: UITableView!
+    
+    @IBAction func tapNewBookButton(_ sender: Any) {
+        performSegue(withIdentifier: "ComposeSegue", sender: nil)
+    }
+    
+    var books = [Book]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        BookTableView.tableHeaderView = UIView()
+        BookTableView.dataSource = self
+        BookTableView.delegate = self
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        refreshBooks()
+    }
+    
+    
+    private func refreshBooks() {
+        // 1.
+        let books = Book.getBooks(forKey: "books")
+        
+        // 3.
+        self.books = books
+        // 4.
+        EmptyStateLabel.isHidden = !books.isEmpty
+        // 5.
+        BookTableView.reloadSections(IndexSet(integer: 0), with: .automatic)
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+// Table View Data Source Methods
+extension BookListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as! BookCell
+                
+        return cell
+    }
+    
+    //delete functionality
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // 1.
+        if editingStyle == .delete {
+            // 2.
+            books.remove(at: indexPath.row)
+            // 3.
+            Book.save(books, forKey: "books")
+            // 4.
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+}
 
+
+// Table View Delegate Methods
+extension BookListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: false)
+        
+        let selectedBook = books[indexPath.row]
+        
+        performSegue(withIdentifier: "ComposeSegue", sender: selectedBook)
+    }
 }
